2a87d5454f046561c60be3204c0e7352b5c80a33
Ethan <ethanjsummers00@gmail.com>
2023-01-21 17:22:58 -0700

    Functioning Close Button for Cart
    - Added close button that appears and disappears in the menu
    - Added section for deals below departments

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ function openCart() {
     document.getElementById("cartNav").style.width = "25%";
     document.querySelector("main").style.marginRight = "25%";
     document.querySelector("header").style.marginRight= "25%";
+    document.getElementById("close").style.display = "block";
 }
 
 function closeCart() {
     document.getElementById("cartNav").style.width = "0";
     document.querySelector("main").style.marginRight= "0";
     document.querySelector("header").style.marginRight= "0";
+    document.getElementById("close").style.display = "none";
 }
